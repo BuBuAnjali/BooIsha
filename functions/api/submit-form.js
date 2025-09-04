dd493b23a8f01a6a3e5c9754702b76500a7d9608
BuBu <gayathrianajali06@gmail.com>
2025-09-04 21:51:46 +1000

    email update

--- functions/api/submit-form.js
+++ functions/api/submit-form.js
@@ -60,7 +60,7 @@ export async function onRequestPost(context) {
         },
       ],
       from: {
-        email: "noreply@771e5f39.booshiv.pages.dev",
+        email: "noreply@booshiv.pages.dev",
         name: "BooShiv Website",
       },
       subject: `New Enquiry: ${data.subject}`,
